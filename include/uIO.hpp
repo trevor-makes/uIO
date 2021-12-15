b6b27bcfd2ced15ee2fb1f30278d217d88e3e3f9
Trevor Makes <the.trevor.makes@gmail.com>
2021-12-14 20:50:49 -0800

    Remove Port16 lsb/msb write

--- include/uIO.hpp
+++ include/uIO.hpp
@@ -137,20 +137,10 @@ struct Port16 {
     PortLSB::bitwise_and(value & 0xFF);
   }
 
-  // Write upper byte of value to upper port
-  static void write_msb(uint16_t value) {
-    PortMSB::write(value / 0x100);
-  }
-
-  // Write lower byte of value to lower port
-  static void write_lsb(uint16_t value) {
-    PortLSB::write(value & 0xFF);
-  }
-
   // Write 16-bit value to high and low ports
   static void write(uint16_t value) {
-    write_msb(value);
-    write_lsb(value);
+    PortMSB::write(value / 0x100);
+    PortLSB::write(value & 0xFF);
   }
 
   // Read 16-bit value from high and low ports
