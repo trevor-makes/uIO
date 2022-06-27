3fe826eebe40d8b01135f4a9c9e80d9818411116
Trevor Makes <the.trevor.makes@gmail.com>
2022-06-27 02:17:14 -0700

    Use derived type throughout uIO_REG

--- include/uIO.hpp
+++ include/uIO.hpp
@@ -247,36 +247,38 @@ struct Port16 {
 };
 
 #define uIO_REG(REG) \
+  using TYPE_##REG = RemoveVolatileRef<decltype((REG))>::TYPE; \
+  \
   template <uint8_t BIT, \
-    uint8_t MASK = 1 << BIT> \
+    TYPE_##REG MASK = 1 << BIT> \
   struct RegBit##REG; \
   \
-  template <uint8_t MASK, \
-    bool B = (MASK == 0xFF)> \
+  template <TYPE_##REG MASK, \
+    bool B = (MASK == TYPE_##REG(~0))> \
   struct RegMask##REG; \
   \
-  using Reg##REG = RegMask##REG<0xFF>; \
+  using Reg##REG = RegMask##REG<TYPE_##REG(~0)>; \
   \
-  template <uint8_t M> \
+  template <TYPE_##REG M> \
   struct RegBase##REG { \
-    using TYPE = RemoveVolatileRef<decltype((REG))>::TYPE; \
-    static const uint8_t MASK = M; \
+    using TYPE = TYPE_##REG; \
+    static const TYPE MASK = M; \
     /* Select single bit within register */ \
     template <uint8_t BIT> \
     using Bit = RegMask##REG<1 << BIT>; \
     /* Select masked subfield within register */ \
-    template <uint8_t SUBMASK> \
+    template <TYPE SUBMASK> \
     using Mask = RegMask##REG<SUBMASK>; \
   }; \
   \
   /* Unmasked register operations */ \
-  template <uint8_t MASK> \
+  template <TYPE_##REG MASK> \
   struct RegMask##REG<MASK, true> : RegBase##REG<MASK> { \
-    static_assert(MASK == 0xFF, \
-      "Unmasked register should have MASK 0xFF"); \
+    static_assert(MASK == TYPE_##REG(~0), \
+      "Unmasked register should have all MASK bits set"); \
     struct Input { \
       /* Read from I/O register; emits [IN] */ \
-      static inline uint8_t read() { return (REG); } \
+      static inline TYPE_##REG read() { return (REG); } \
       /* Test if bits are clear; `if (clear)` may emit [SBIS] */ \
       static inline bool is_clear() { return (REG) == 0; } \
       /* Test if bits are set; `if (set)` may emit [SBIC] */ \
@@ -289,13 +291,13 @@ struct Port16 {
       /* Set bits; emits [(LDI) OUT] */ \
       static inline void set() { (REG) = MASK; } \
       /* Clear bits; emits [IN ANDI OUT] or [CBI] */ \
-      static inline void clear() { (REG) = uint8_t(~MASK); } \
+      static inline void clear() { (REG) = TYPE_##REG(~MASK); } \
       /* Write to I/O register; emits [(LDI) OUT] */ \
-      static inline void write(uint8_t value) { (REG) = value; } \
+      static inline void write(TYPE_##REG value) { (REG) = value; } \
       /* Apply bitwise OR; emits [IN, OR, OUT] */ \
-      static inline void bitwise_or(uint8_t value) { (REG) |= value; } \
+      static inline void bitwise_or(TYPE_##REG value) { (REG) |= value; } \
       /* Apply bitwise AND; emits [IN, AND, OUT] */ \
-      static inline void bitwise_and(uint8_t value) { (REG) &= value; } \
+      static inline void bitwise_and(TYPE_##REG value) { (REG) &= value; } \
     }; \
     static constexpr auto set = &Output::set; \
     static constexpr auto clear = &Output::clear; \
@@ -305,13 +307,13 @@ struct Port16 {
   }; \
   \
   /* Masked register operations */ \
-  template <uint8_t MASK> \
+  template <TYPE_##REG MASK> \
   struct RegMask##REG<MASK, false> : RegBase##REG<MASK> { \
-    static_assert(MASK != 0 && MASK != 0xFF, \
-      "Masked register should have non-zero MASK less than 0xFF"); \
+    static_assert(MASK != 0 && MASK != TYPE_##REG(~0), \
+      "Masked register should have some but not all MASK bits set"); \
     struct Input { \
       /* Read from I/O register; emits IN, ANDI */ \
-      static inline uint8_t read() { return (REG) & MASK; } \
+      static inline TYPE_##REG read() { return (REG) & MASK; } \
       /* Test if bits are clear; `if (clear)` may emit [SBIS] */ \
       static inline bool is_clear() { return ((REG) & MASK) == 0; } \
       /* Test if bits are set; `if (set)` may emit [SBIC] */ \
@@ -324,13 +326,13 @@ struct Port16 {
       /* Set bits; emits [IN, ORI, OUT] or [SBI] */ \
       static inline void set() { (REG) |= MASK; } \
       /* Clear bits; emits [IN ANDI OUT] or [CBI] */ \
-      static inline void clear() { (REG) &= uint8_t(~MASK); } \
+      static inline void clear() { (REG) &= TYPE_##REG(~MASK); } \
       /* Write to I/O register; emits IN, ANDI, (ANDI,) OR, OUT */ \
-      static inline void write(uint8_t value) { (REG) = ((REG) & uint8_t(~MASK)) | (value & MASK); } \
+      static inline void write(TYPE_##REG value) { (REG) = ((REG) & TYPE_##REG(~MASK)) | (value & MASK); } \
       /* Apply bitwise OR; emits to IN, (ANDI,) OR, OUT */ \
-      static inline void bitwise_or(uint8_t value) { (REG) |= value & MASK; } \
+      static inline void bitwise_or(TYPE_##REG value) { (REG) |= value & MASK; } \
       /* Apply bitwise AND; emits to IN, (ORI,) AND, OUT */ \
-      static inline void bitwise_and(uint8_t value) { (REG) &= value | uint8_t(~MASK); } \
+      static inline void bitwise_and(TYPE_##REG value) { (REG) &= value | TYPE_##REG(~MASK); } \
     }; \
     static constexpr auto set = &Output::set; \
     static constexpr auto clear = &Output::clear; \
