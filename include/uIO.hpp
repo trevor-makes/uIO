c569be206ae0cc7eeaf88c5b6bc98130f38f5609
Trevor Makes <the.trevor.makes@gmail.com>
2022-07-02 02:39:11 -0700

    Add ilog2 and countl_zero

--- include/uIO.hpp
+++ include/uIO.hpp
@@ -71,6 +71,53 @@ static_assert(countr_zero(0x80000000) == 31);
 static_assert(countr_zero(0x8000000000000000) == 63);
 static_assert(countr_zero(0xF0F0F0F0) == 4);
 
+template <typename T>
+constexpr T less_one_until_zero(T N) { return N > 0 ? N - 1 : 0; }
+
+//http://graphics.stanford.edu/~seander/bithacks.html#IntegerLog
+template <uint8_t N, typename T, T S = T(1) << N, T P = ((T(1) << S) - 1)>
+constexpr uint8_t ilog2_impl(T v) {
+  return N > 0 ? (v > P) << N | ilog2_impl<less_one_until_zero(N)>(v >> ((v > P) << N)) : (v > P);
+}
+
+template <typename T>
+constexpr uint8_t ilog2(T v) {
+  return ilog2_impl<2 + ilog2_impl<1>(sizeof(T))>(v);
+}
+
+static_assert(ilog2(0) == 0);
+static_assert(ilog2(1) == 0);
+
+static_assert(ilog2(0x40) == 6);
+static_assert(ilog2(0x55) == 6);
+static_assert(ilog2(0x7F) == 6);
+
+static_assert(ilog2(0x80) == 7);
+static_assert(ilog2(0xCC) == 7);
+static_assert(ilog2(0xFF) == 7);
+
+static_assert(ilog2(0xFFFF) == 15);
+static_assert(ilog2(0xFFFFFF) == 23);
+static_assert(ilog2(0xFFFFFFFF) == 31);
+static_assert(ilog2(0xFFFFFFFFFFFF) == 47);
+static_assert(ilog2(0xFFFFFFFFFFFFFFFF) == 63);
+
+// NOTE std::countl_zero added to <bit> in c++20
+template <typename T>
+constexpr uint8_t countl_zero(T v) {
+  return sizeof(T) * 8 - (v > 0 ? ilog2(v) + 1 : 0);
+}
+
+static_assert(countl_zero<uint16_t>(0x0000) == 16);
+static_assert(countl_zero<uint16_t>(0x0001) == 15);
+static_assert(countl_zero<uint16_t>(0x7FFF) == 1);
+static_assert(countl_zero<uint16_t>(0x8000) == 0);
+
+static_assert(countl_zero(uint32_t(0)) == 32);
+static_assert(countl_zero(uint32_t(1)) == 31);
+static_assert(countl_zero(uint32_t(0x80000000)) == 0);
+static_assert(countl_zero(uint32_t(0xF0F0F0F0)) == 0);
+
 } // namespace util
 
 namespace uIO {
